b8a9f907508497ce5405693cc7bd9519671e1c23
‎ <>
2025-01-31 06:28:34 +0700

    up

--- package/general_framework/template/base_template_general_framework_project/library/base_template_general_framework_project_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ package/general_framework/template/base_template_general_framework_project/library/base_template_general_framework_project_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,24 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import database_universe_flutter_libs
 import flutter_tts
-import local_auth_darwin
-import media_kit_video
+import media_kit_video_fork
 import package_info_plus
 import path_provider_foundation
 import screen_brightness_macos
+import screen_retriever_macos
 import speech_to_text
 import url_launcher_macos
 import wakelock_plus
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DatabaseUniverseFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "DatabaseUniverseFlutterLibsPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
